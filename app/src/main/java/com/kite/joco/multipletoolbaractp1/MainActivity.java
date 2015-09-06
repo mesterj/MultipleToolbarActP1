b1b7cbac9b063f3bf448b2ab3a56a70e2c5d30b4
mesterj <mesterj@freemail.hu>
2015-09-06 20:14:45 +0200

    second commit

--- app/src/main/java/com/kite/joco/multipletoolbaractp1/MainActivity.java
+++ app/src/main/java/com/kite/joco/multipletoolbaractp1/MainActivity.java
@@ -1,22 +1,66 @@
 package com.kite.joco.multipletoolbaractp1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
+
+    TextView tvHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar tlbMain = (Toolbar) findViewById(R.id.tlbMain);
+        tvHello = (TextView) findViewById(R.id.tvHello);
+
+        setSupportActionBar(tlbMain);
+        tlbMain.inflateMenu(R.menu.menu_main);
+        if (getIntent() != null) {
+            handleIntent(getIntent());
+        } else {
+            Log.d("TOOLBAR:MAINACTIVITY", ":Hogy a csába lehet null az intent???");
+        }
+
     }
+
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Log.d("TOOLBARSEARCH", " Search fired with : " + query);
+            showsearchString(query);
+        } else {
+            Log.d("TOOLBARSEARCH", " Nem kereséssel lett az Activity meghívva");
+        }
+    }
+
+    private void showsearchString(String text) {
+        tvHello.setText(text);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.searhchMain).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setOnQueryTextListener(this);
         return true;
     }
 
@@ -33,5 +77,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String s) {
+        Toast.makeText(this," A beírt kereső szöveg: " + s,Toast.LENGTH_LONG).show();
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        return false;
     }
 }
